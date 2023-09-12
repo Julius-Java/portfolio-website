d51c23c780f2f4c2356dec6fe2baf2f38953cf5c
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-09-12 13:38:59 +0100

    Fix Hire me button on index page

--- components/HireMe.tsx
+++ components/HireMe.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CircularTextSVG } from './Icons'
+
 import Link from 'next/link'
 
 const HireMe = () => {
@@ -8,7 +9,10 @@ const HireMe = () => {
             <div className='w-48 h-auto flex justify-center items-center relative'>
                 <CircularTextSVG className='fill-dark animate-spin-slow'/>
                 <div className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                    <Link href={"mailto:juliusjava00@gmail.com"} className='flex justify-center items-center text-light font-bold p-2 rounded-full bg-dark'>
+                    <Link
+                        href={"mailto:juliusjava00@gmail.com"}
+                        className='flex justify-center items-center text-light font-bold p-2 w-20 h-20 rounded-full bg-dark'
+                    >
                         Hire me
                     </Link>
                 </div>

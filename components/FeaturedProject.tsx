5463c9b76ee66b06647cc08a8a9ce28e786b525c
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2024-01-03 23:29:06 +0100

    Update project portfolio

--- components/FeaturedProject.tsx
+++ components/FeaturedProject.tsx
@@ -1,66 +1,77 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { GithubSVG } from './Icons'
-import {motion} from "framer-motion"
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { GithubSVG } from "./Icons";
+import { motion } from "framer-motion";
 
 type FeaturedProjectProps = {
-    type: string
-    title: string
-    summary: string
-    img: any
-    link: string
-    github: string
-}
+    type: string;
+    title: string;
+    summary: string;
+    img: any;
+    link: string;
+    github?: string;
+};
 
 // const motionImage = motion()
 
-const FeaturedProject = ({type, title, summary, img, link, github}: FeaturedProjectProps) => {
+const FeaturedProject = ({
+    type,
+    title,
+    summary,
+    img,
+    link,
+    github,
+}: FeaturedProjectProps) => {
     return (
-        <article
-            className='flex p-12 items-center justify-between rounded-3xl border border-dark bg-light shadow-2xl relative dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4'
-        >
+        <article className="flex p-12 items-center justify-between rounded-3xl border border-dark bg-light shadow-2xl relative dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
             {/* <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />*/}
             <Link
                 href={link}
-                target='_blank'
-                className='w-1/2 cursor-pointer rounded-lg overflow-hidden lg:w-full'
+                target="_blank"
+                className="w-1/2 cursor-pointer rounded-lg overflow-hidden lg:w-full"
             >
-                    <Image className='transition-all duration-300 hover:scale-110' src={img} alt={title} />
+                <Image
+                    className="transition-all duration-300 hover:scale-110"
+                    src={img}
+                    alt={title}
+                />
             </Link>
 
-            <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
-                <span className='text-primary dark:text-primaryDark font-medium text-xl xs:text-base'>{type}</span>
+            <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+                <span className="text-primary dark:text-primaryDark font-medium text-xl xs:text-base">
+                    {type}
+                </span>
                 <Link
                     href={link}
-                    target='_blank'
-                    className='hover:underline dark:text-light'
+                    target="_blank"
+                    className="hover:underline dark:text-light"
                 >
-                    <h3 className='my-2 w-full text-left text-4xl font-bold sm:text-sm'>{title}</h3>
+                    <h3 className="my-2 w-full text-left text-4xl font-bold sm:text-sm">
+                        {title}
+                    </h3>
                 </Link>
-                <p className='my-2 font-medium text-dark dark:text-light sm:text-sm'>{summary}</p>
+                <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">
+                    {summary}
+                </p>
 
-                <div className='mt-2 flex items-center gap-3'>
-                    <Link
-                        href={github}
-                        target='_blank'
-                        className='w-10'
-                    >
-                        <GithubSVG />
-                    </Link>
-
+                <div className="mt-2 flex items-center gap-3">
+                    {github && (
+                        <Link href={github} target="_blank" className="w-10">
+                            <GithubSVG />
+                        </Link>
+                    )}
                     <Link
                         href={link}
-                        target='_blank'
-                        className='rounded-lg bg-dark text-white py-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base'
+                        target="_blank"
+                        className="rounded-lg bg-dark text-white py-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base"
                     >
                         View Project
                     </Link>
                 </div>
             </div>
-
         </article>
-    )
-}
+    );
+};
 
-export default FeaturedProject
+export default FeaturedProject;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,124 +1,121 @@
-import React from 'react'
-import Head from 'next/head'
-import AnimatedHeading from '@/components/AnimatedHeading'
-import FeaturedProject from '@/components/FeaturedProject'
-import Project from '@/components/Project'
-import watt from "../public/screenshots/wattScreenShot.png"
-import getLinked from "../public/screenshots/glh.png"
-import ageCalculator from "../public/screenshots/ageCalculator.png"
-import interactiveCard from "../public/screenshots/interactiveCardForm.png"
-import noteBlitz from "../public/screenshots/noteBlitzDark.png"
-import notificationPage from "../public/screenshots/notificationPage.png"
-import movieWebsite from "../public/screenshots/movieWeb.png"
-import TransitionEffect from '@/components/TransitionEffect'
+import React from "react";
+import Head from "next/head";
+import AnimatedHeading from "@/components/AnimatedHeading";
+import FeaturedProject from "@/components/FeaturedProject";
+import Project from "@/components/Project";
+import watt from "../public/screenshots/wattScreenShot.png";
+import getLinked from "../public/screenshots/glh.png";
+import ageCalculator from "../public/screenshots/ageCalculator.png";
+import interactiveCard from "../public/screenshots/interactiveCardForm.png";
+import noteBlitz from "../public/screenshots/noteBlitzDark.png";
+import notificationPage from "../public/screenshots/notificationPage.png";
+import movieWebsite from "../public/screenshots/movieWeb.png";
+import TransitionEffect from "@/components/TransitionEffect";
+import sc from "../public/screenshots/scshot.png";
 
 const Projects = () => {
     return (
         <>
             <Head>
                 <title>Julius Java | Projects</title>
-                <meta name='description' content="Julius Java's Projects Showcase." />
+                <meta
+                    name="description"
+                    content="Julius Java's Projects Showcase."
+                />
             </Head>
             <TransitionEffect />
-            <main className='p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 dark:text-white'>
+            <main className="p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 dark:text-white">
                 <AnimatedHeading
                     text="Exploring Innovation: <br/> A Showcase of My Projects"
-                    className='text-black text-7xl sm:text-5xl xs:text-3xl  mb-14 font-extrabold w-full text-center dark:text-light'
+                    className="text-black text-7xl sm:text-5xl xs:text-3xl  mb-14 font-extrabold w-full text-center dark:text-light"
                 />
 
-                <div
-                    className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'
-                >
-                    <div
-                        className='col-span-12'
-                    >
+                <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+                    <div className="col-span-12">
                         <FeaturedProject
-                            title='GetLinked Hackathon'
-                            summary='Fully functioal website for signing up/ registering for the GetLinked Hackathon as well as contact support page. Built with Next JS, Tailwind CSS and Daisy UI'
-                            link='https://getlinked-hackathon-chi.vercel.app/'
-                            type='Featured Project'
+                            title="Student Corner"
+                            summary="The landing page for a student community platform. Built with Next JS, Tailwind CSS, Next UI and Firebase for waitlist data storage"
+                            link="https://studentcorner.live/"
+                            type="Featured Project"
+                            img={sc}
+                        />
+                    </div>
+                    <div className="col-span-12">
+                        <FeaturedProject
+                            title="GetLinked Hackathon"
+                            summary="Fully functioal website for signing up/ registering for the GetLinked Hackathon as well as contact support page. Built with Next JS, Tailwind CSS and Daisy UI"
+                            link="https://getlinked-hackathon-chi.vercel.app/"
+                            type="Featured Project"
                             img={getLinked}
-                            github='https://github.com/Julius-Java/getlinkedHackathon'
+                            github="https://github.com/Julius-Java/getlinkedHackathon"
                         />
                     </div>
 
-                    <div
-                        className='col-span-12'
-                    >
+                    <div className="col-span-12">
                         <FeaturedProject
-                            title='Watt'
-                            summary='Frontend for a utility payment platform built with React JS and CSS'
-                            link='https://watt-v2.vercel.app/'
-                            type='Featured Project'
+                            title="Watt"
+                            summary="Frontend for a utility payment platform built with React JS and CSS"
+                            link="https://watt-v2.vercel.app/"
+                            type="Featured Project"
                             img={watt}
-                            github='https://github.com/Julius-Java/Watt-v2'
+                            github="https://github.com/Julius-Java/Watt-v2"
                         />
                     </div>
 
-                    <div
-                        className='col-span-6 sm:col-span-12'
-                    >
+                    <div className="col-span-6 sm:col-span-12">
                         <Project
-                            title='Interactive Card Form'
-                            link='https://interactive-card-form-five.vercel.app/'
-                            type='Personal Project'
+                            title="Interactive Card Form"
+                            link="https://interactive-card-form-five.vercel.app/"
+                            type="Personal Project"
                             img={interactiveCard}
-                            github='https://github.com/Julius-Java/interactive-card-form'
+                            github="https://github.com/Julius-Java/interactive-card-form"
                         />
                     </div>
 
-                    <div
-                        className='col-span-6 sm:col-span-12'
-                    >
+                    <div className="col-span-6 sm:col-span-12">
                         <Project
-                            title='Age Calculator'
-                            link='https://age-calculatore.vercel.app/'
-                            type='Personal Project'
+                            title="Age Calculator"
+                            link="https://age-calculatore.vercel.app/"
+                            type="Personal Project"
                             img={ageCalculator}
-                            github='https://github.com/Julius-Java/ageCalculatore'
+                            github="https://github.com/Julius-Java/ageCalculatore"
                         />
                     </div>
 
-                    <div
-                        className='col-span-12'
-                    >
+                    <div className="col-span-12">
                         <FeaturedProject
-                            title='Note Blitz'
-                            summary='A minimal Todo web app built with Next JS, Tailwind CSS and Daisy UI. It stores user todo in browser local storage for persistence. Users can create multiple categories for different todos e.g School, Business, Personal etc. It also has a dark mode. The note section where users would be able to write notes is still under development.'
-                            link='https://note-blitz.vercel.app/'
-                            type='Featured Project'
+                            title="Note Blitz"
+                            summary="A minimal Todo web app built with Next JS, Tailwind CSS and Daisy UI. It stores user todo in browser local storage for persistence. Users can create multiple categories for different todos e.g School, Business, Personal etc. It also has a dark mode. The note section where users would be able to write notes is still under development."
+                            link="https://note-blitz.vercel.app/"
+                            type="Featured Project"
                             img={noteBlitz}
-                            github='https://github.com/Julius-Java/noteBlitz'
+                            github="https://github.com/Julius-Java/noteBlitz"
                         />
                     </div>
 
-                    <div
-                        className='col-span-6 sm:col-span-12'
-                    >
+                    <div className="col-span-6 sm:col-span-12">
                         <Project
-                            title='Notification Page'
-                            link='https://notification-page-weld-three.vercel.app/'
-                            type='Personal Project'
+                            title="Notification Page"
+                            link="https://notification-page-weld-three.vercel.app/"
+                            type="Personal Project"
                             img={notificationPage}
-                            github='https://github.com/Julius-Java/notification-page'
+                            github="https://github.com/Julius-Java/notification-page"
                         />
                     </div>
 
-                    <div
-                        className='col-span-6 sm:col-span-12'
-                    >
+                    <div className="col-span-6 sm:col-span-12">
                         <Project
-                            title='Movie Web App'
-                            link='https://movie-app-ten-gold.vercel.app/'
-                            type='Personal Project'
+                            title="Movie Web App"
+                            link="https://movie-app-ten-gold.vercel.app/"
+                            type="Personal Project"
                             img={movieWebsite}
-                            github='https://github.com/Julius-Java/movie-app'
+                            github="https://github.com/Julius-Java/movie-app"
                         />
                     </div>
                 </div>
             </main>
         </>
-    )
-}
+    );
+};
 
-export default Projects
+export default Projects;

c1f415b464bf58975c4b34b20550de6945b0cc5b
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2025-03-10 15:17:15 +0100

    add comment to server action

--- services/actions.ts
+++ services/actions.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 import {Resend} from "resend"
 import ContactFormEmail from "@/emails/email";
 
+// Include env in production
 const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
 
 export async function sendEmail(data: z.infer<typeof formSchema>) {
